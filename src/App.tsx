4e7430c57dbe5265f57302f50f25a2a24616dad4
KarthikeyaThallapally <karthikeya.gsi@gmail.com>
2025-06-12 11:55:47 +0530

    Create missing React source files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center py-8">
+        MysticIndicators.com
+      </h1>
+      <p className="text-center text-gray-600">
+        Website is loading...
+      </p>
+    </div>
+  )
+}
+
+export default App
